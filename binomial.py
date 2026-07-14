cc6b36c060d50c62edf50dd648ab62ec59e8c356
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-15 04:07:44 +0700

    Binomial SAT NQeens Solution

---
+++ binomial.py
@@ -0,0 +1,67 @@
+from pysat.solvers import Glucose3
+
+
+def generate_variables(n):
+    return [[i * n + j + 1 for j in range(n)] for i in range(n)]
+
+
+def at_most_one(clauses, variables):
+    for i in range(0, len(variables)):
+        for j in range(i + 1, len(variables)):
+            clauses.append([-variables[i], -variables[j]])
+    return clauses
+
+
+def exactly_one(clauses, variables):
+    clauses.append(variables)
+    at_most_one(clauses, variables)
+
+
+def generate_clauses(n, variables):
+    clauses = []
+
+    for i in range(n):
+        exactly_one(clauses, variables[i])
+
+    for j in range(n):
+        exactly_one(clauses, [variables[i][j] for i in range(n)])
+
+    for i in range(n):
+        for j in range(n):
+            for k in range(1, n):
+                if i + k < n and j + k < n:
+                    at_most_one(clauses, [variables[i][j], variables[i + k][j + k]])
+                if i + k < n and j - k >= 0:
+                    at_most_one(clauses, [variables[i][j], variables[i + k][j - k]])
+
+    return clauses
+
+
+def solve_n_queens(n):
+    variables = generate_variables(n)
+    clauses = generate_clauses(n, variables)
+    print(clauses)
+
+    solver = Glucose3()
+    for clause in clauses:
+        solver.add_clause(clause)
+
+    if solver.solve():
+        model = solver.get_model()
+        return [[int(model[i * n + j] > 0) for j in range(n)] for i in range(n)]
+    else:
+        return None
+
+
+def print_solution(solution):
+    if solution is None:
+        print("No solution found.")
+    else:
+        print(solution)
+        for row in solution:
+            print(" ".join("Q" if cell else "." for cell in row))
+
+
+n = 8
+solution = solve_n_queens(n)
+print_solution(solution)
