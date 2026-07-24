afb912a1ac6c04bcd58a5387b31b3295e07b5b12
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-24 15:22:40 +0700

    Ptypes.hh BCP SAT Encoding

---
+++ Bandwidth_Coloring_Problem/card_encoder/ptypes.hh
@@ -0,0 +1,28 @@
+#ifndef PTYPES_HH_
+#define PTYPES_HH_
+
+#include <unordered_map>
+
+using namespace std;
+
+typedef pair<int, int> IntPair;
+
+class IntPairHash {
+public:
+	int operator() (IntPair pval) const
+	{
+		return pval.first * pval.second;
+	}
+};
+
+class IntPairEqual {
+public:
+	bool operator() (IntPair p1, IntPair p2) const
+	{
+		return p1.first == p2.first && p1.second == p2.second;
+	}
+};
+
+typedef unordered_map<IntPair, int, IntPairHash, IntPairEqual> Pair2IntMap;
+
+#endif // PTYPES_HH_
